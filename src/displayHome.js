d1667abb85f6648aea430d13dd773efcbbe41331
kiizerd <ajsewell13@gmail.com>
2021-03-15 00:25:17 -0500

    first commit for displayHome modules

---
+++ src/displayHome.js
@@ -0,0 +1,19 @@
+const Display = (function () {
+  
+
+
+  function getFeatured() {
+    const div = document.createElement('div');
+    div.classList.add('card-group');
+
+    const firstItem = getFeateuredItem(1)
+    const secondItem = getFeateuredItem(2)
+    const thirdItem = getFeateuredItem(3)
+  };
+
+  function getFeaturedItem(itemNum) {
+    const div = document.createElement('div');
+  }
+
+  return {}
+})();
