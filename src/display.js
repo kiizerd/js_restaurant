6c8b982d9be0b22e559706deccf849bbb6bede13
kiizerd <ajsewell13@gmail.com>
2021-03-26 00:26:52 -0500

    isolated display module

---
+++ src/display.js
@@ -0,0 +1,13 @@
+import { getHeader } from './displayHeader'
+import { getHomePage } from './displayHome'
+import { getMenuPage } from "./displayMenu";
+import { getAboutPage } from "./displayAbout";
+
+const Display = {
+  getHeader,
+  getHomePage,
+  getMenuPage,
+  getAboutPage
+}
+
+export { Display }
