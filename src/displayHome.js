7c174cdb1e41c45af927b658a375a4282a3a7d4d
kiizerd <ajsewell13@gmail.com>
2021-03-26 00:30:19 -0500

    renamed function

--- src/displayHome.js
+++ src/displayHome.js
@@ -1,19 +1,65 @@
 const Display = (function () {
   
-
-
-  function getFeatured() {
+  function getHomePage() {
     const div = document.createElement('div');
-    div.classList.add('card-group');
+    div.classList.add('container-fluid')
 
-    const firstItem = getFeateuredItem(1)
-    const secondItem = getFeateuredItem(2)
-    const thirdItem = getFeateuredItem(3)
-  };
+    const cardColumn = getCardColumn();
 
-  function getFeaturedItem(itemNum) {
-    const div = document.createElement('div');
+    div.append(cardColumn);
+
+    return div;
   }
 
-  return {}
+  function getCardColumn() {
+    const div = document.createElement('div');
+    div.classList.add('card-columns');
+
+    const featured = getFeaturedCard();
+    const amenities = getAmenitiesCard();
+    const locations = getLocationsCard();
+
+    div.append(featured, amenities, locations)
+
+    return div
+  }
+
+  function getFeaturedCard() {
+    const div = document.createElement('div');
+    
+    div.append(getHeader())
+        
+    return div
+
+    function getHeader() {
+      const header = document.createElement('div')
+      header.classList.add('card-header');
+      header.textContent = 'Featured items'
+    
+      return header
+    }
+  };
+
+  function getFeaturedItem(itemNum) {const div = document.createElement('div');
+
+  return div
+  };
+
+  function getAmenitiesCard() {
+    const div = document.createElement('div');
+
+    return div
+  };
+  
+  function getLocationsCard() {
+    const div = document.createElement('div');
+
+    return div
+  };
+
+  return { getHomePage }
 })();
+
+const getHomePage = Display.getHomePage;
+
+export { getHomePage };
